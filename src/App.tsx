86444c42122508acbf3e2bd46c59ee2773197d9c
Ka Hung Lee <lee.kahung414@gmail.com>
2022-10-22 03:27:32 -0700

    Inital build and styling of calendar for event calendar

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,18 @@ function App() {
       </div>
       <div className="calendar-ctnr">
         <h1>Month</h1>
-        <div className="calendar-grid"></div>
+        <div className="calendar-grid">
+          {["Sunday", "Monday", "Tuesday", "Wednesday", "Thursaday", "Friday", "Saturday"].map((item) => {
+            return (item === "Sunday" || item === "Saturday")
+              ? (React.createElement("div", { className: "day-name weekend"}, item))
+              : (React.createElement("div", { className: "day-name"}, item))
+          })}
+          {[...Array(35)].map((item, index) => {
+            return ((index + 1) % 7 === 1 || (index + 1) % 7 === 0)
+              ? (React.createElement("div", { className: "day first-col"}))
+              : (React.createElement("div", { className: "day"}))
+          })}
+        </div>
       </div>
     </div>
   );
