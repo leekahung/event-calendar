e348c9d895ee27260e936ba396b7cec329fc65ef
Ka Hung Lee <lee.kahung414@gmail.com>
2022-10-27 18:01:30 -0700

    Included a menu button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Calendar from "./components/calendar/Calendar";
+import menuIcon from "./assets/img/menu.png"
 
 function App() {
   const today = new Date();
 
+  const [toggled, setToggled] = useState(false);
+
+  const handleClick = () => {
+    const menuBtn = document.getElementById("menu-icon") as HTMLButtonElement;
+    const navBar = document.querySelector(".navbar") as HTMLElement;
+    const calendarCtnr = document.querySelector(".calendar-ctnr") as HTMLDivElement;
+    let menuToggled = false;
+
+    menuBtn.addEventListener("click", () => {
+      if (toggled === false) {
+        navBar.style.width = "0";
+        calendarCtnr.style.width = "100%";
+        setToggled(true);
+      } else {
+        navBar.style.width = "20%";
+        calendarCtnr.style.width = "80%";
+        setToggled(false);
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <div className="navbar">
-      </div>
+      <nav className="navbar">
+        <button id="menu-icon" onClick={handleClick}>
+          <img src={menuIcon} alt="burger menu icon" />
+        </button>
+      </nav>
       <Calendar date={today}/>
     </div>
   );
